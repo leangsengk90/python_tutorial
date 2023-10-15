f43ac8a01c37a4b30afee69e95186a762cddb6e2
kao.leangseng <kao.leangseng@ababank.com>
2023-10-15 17:55:58 +0700

    change

--- 11-exception.py
+++ 11-exception.py
@@ -1,6 +1,7 @@
 try:
     int(input("Give me you age: "))
-    print("Damn! Cool!")
-except:
+    print("Damn! Cool!", 100/0)
+except ValueError:
     print("Watch your input!")
-    
+except Exception as ex:
+    print("Catch all exception!", ex)
