c721b0d16ae03128ef1e4195e58b5b33a6a03baa
kao.leangseng <kao.leangseng@ababank.com>
2023-10-15 20:24:16 +0700

    function return as turple

--- modules/my_greeting.py
+++ modules/my_greeting.py
@@ -1,3 +1,3 @@
 def greeting(first_name, last_name):
     print("Welcome!", f"{first_name} {last_name}")
-    return "Boom" # Default: return None
+    return "Boom", 123 # Default: return None; Python can return muli value as turple
