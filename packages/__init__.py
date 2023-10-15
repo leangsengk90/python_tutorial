8179c95ef934dbd69cf43c15868066bf7f293e99
kao.leangseng <kao.leangseng@ababank.com>
2023-10-15 20:22:40 +0700

    change package

--- packages/__init__.py
+++ packages/__init__.py
@@ -1,6 +1,6 @@
 # from packages.shopping import get_shipping # Error wrong path
-# from shopping import get_shipping, drag_location
-from shopping import *
+from shopping import get_shipping, drag_location as drag
+# from shopping import *
 
 get_shipping()
-drag_location()
+drag()
